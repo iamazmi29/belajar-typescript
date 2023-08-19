94067db0f227610843b71b5fa2f6b2f300921d53
iamazmi29 <imza.al29@gmail.com>
2023-08-19 13:48:22 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div>
+       
+           <h1 className="text-3xl bg-red-200 font-bold underline">
+      Hello world!
+    </h1>
+
+      </div>
+     
+    </>
+  )
+}
+
+export default App
